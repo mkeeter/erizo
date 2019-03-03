4f6660b1dbd315e8c766935da6975b1614c6adf4
Matt Keeter <matt.j.keeter@gmail.com>
2019-03-03 10:10:29 -0500

    Got HashSet working with raw pointer keys

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 extern crate nalgebra as na;
 
+use failure::{Error, err_msg};
+use fnv::FnvHashSet;
 use nom::types::CompleteByteSlice;
 use nom::{le_f32, le_u32, named, do_parse, call, take};
-
-use failure::{Error, err_msg};
-
 use memmap::MmapOptions;
 
+use std::collections::HashSet;
+use std::cmp::Ordering;
 use std::fs::File;
+use std::hash::{Hash, Hasher};
 use std::mem::size_of;
-use std::collections::{HashMap, HashSet};
-use std::hash::{BuildHasher, BuildHasherDefault, Hasher};
-use fnv::FnvHasher;
 
 type Vec3f = na::Vector3<f32>;
 type Vec3i = na::Vector3<u32>;
@@ -40,83 +39,44 @@ named!(parse_header<&[u8], (&[u8], u32)>,
 
 ////////////////////////////////////////////////////////////////////////////////
 
-struct StlBuildHasher<'a> {
-    data: &'a [u8],
-    builder: BuildHasherDefault<FnvHasher>,
-}
-
-impl <'a> StlBuildHasher<'a> {
-    fn new(data: &'a [u8]) -> Self {
-        StlBuildHasher {
-            data: data,
-            builder: BuildHasherDefault::<FnvHasher>::default(),
-        }
+struct RawStl<'a>(&'a [u8]);
+impl<'a> RawStl<'a> {
+    fn key(&self, v: u32) -> StlKey {
+        let triangle = (v / 3) as usize;
+        let vertex = (v % 3) as usize;
+        let i = 80
+            + size_of::<u32>()
+            + triangle * (3 * 4 * size_of::<f32>() + size_of::<u16>())
+            + 3 * size_of::<f32>()
+            + vertex * 3 * size_of::<f32>();
+        StlKey(self.0[i..].as_ptr())
     }
 }
 
-impl<'a> BuildHasher for StlBuildHasher<'a> {
-    type Hasher = StlHasher<'a>;
-    fn build_hasher(&self) -> Self::Hasher {
-        StlHasher {
-            data: self.data,
-            hasher: self.builder.build_hasher(),
-        }
+struct StlKey(*const u8);
+
+impl StlKey {
+    fn get_slice(&self) -> &[u8] {
+        unsafe { std::slice::from_raw_parts(self.0, 12) }
     }
 }
 
-struct StlHasher<'a> {
-    data: &'a [u8],
-    hasher: FnvHasher,
-}
-
-impl<'a> Hasher for StlHasher<'a> {
-    fn write(&mut self, bytes: &[u8]) {
-        unimplemented!();
-    }
-    fn write_u32(&mut self, index: u32) {
-        println!("Hashing {} ({:?})", index, vertex_range(index));
-        println!("{:?}", parse_vec(&self.data[vertex_range(index)]));
-        let i = index as usize;
-        self.hasher.write(&self.data[vertex_range(index)]);
-    }
-    fn finish(&self) -> u64 {
-        println!("finishing");
-        let out = self.hasher.finish();
-        println!("{}", out);
-        out
-    }
-}
-
-fn vertex_range(v: u32) -> std::ops::Range<usize> {
-    let triangle = (v / 3) as usize;
-    let vertex = (v % 3) as usize;
-    let i = 80
-        + size_of::<u32>()
-        + triangle * (3 * 4 * size_of::<f32>() + size_of::<u16>())
-        + 3 * size_of::<f32>()
-        + vertex * 3 * size_of::<f32>();
-    i..i + 3 * size_of::<f32>()
-}
-
-struct StlKey<'a> {
-    v: u32,
-    data: &'a [u8]
-}
-
-impl<'a> PartialEq for StlKey<'a> {
+impl PartialEq for StlKey {
     fn eq(&self, other: &StlKey) -> bool {
-        self.data[vertex_range(self.v)] == self.data[vertex_range(other.v)]
+        self.get_slice() == other.get_slice()
     }
 }
+impl Eq for StlKey {}
 
-impl Borrow<StlKey> for u32 {
-    fn borrow(&self) -> &Id {
-        &self.id
+impl Hash for StlKey {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.get_slice().hash(state);
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/*
 struct Worker<'a> {
     chunk: &'a [u8],
     verts: HashMap<[u8; 12], u32>,
@@ -155,10 +115,11 @@ impl <'a> Worker <'a> {
         }
     }
 }
+*/
 
 fn main() -> Result<(), Error> {
-    //let file = File::open("/Users/mkeeter/Models/porsche.stl")?;
-    let file = File::open("cube.stl")?;
+    let file = File::open("/Users/mkeeter/Models/porsche.stl")?;
+    //let file = File::open("cube.stl")?;
     let mmap = unsafe { MmapOptions::new().map(&file)? };
     println!("Loading stl");
 
@@ -167,11 +128,10 @@ fn main() -> Result<(), Error> {
     let triangle_count = (header.1).1;
     println!("Triangle count: {}", triangle_count);
 
-    let hasher = StlBuildHasher::new(&mmap);
-    let mut set = HashSet::<u32, _>::with_hasher(hasher);
+    let stl = RawStl(&mmap);
+    let mut set: HashSet<StlKey, _> = FnvHashSet::default();
     for v in 0..triangle_count * 3 {
-        let k = StlKey { v: v, data: &mmap };
-        set.get(&k);
+        set.insert(stl.key(v));
     }
     println!("total vertex count: {}", set.len());
 
