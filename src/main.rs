b084f139c9871134c0b9e78fec85a28639ab0e60
Matt Keeter <matt.j.keeter@gmail.com>
2019-03-03 09:52:36 -0500

    Going off the rails here

--- src/main.rs
+++ src/main.rs
@@ -40,47 +40,82 @@ named!(parse_header<&[u8], (&[u8], u32)>,
 
 ////////////////////////////////////////////////////////////////////////////////
 
-struct CustomBuildHasher<'a> {
-    chunk: &'a [u8],
-    fnv_builder: BuildHasherDefault<FnvHasher>,
+struct StlBuildHasher<'a> {
+    data: &'a [u8],
+    builder: BuildHasherDefault<FnvHasher>,
 }
 
-impl <'a> CustomBuildHasher<'a> {
-    fn new(chunk: &'a [u8]) -> CustomBuildHasher<'a> {
-        CustomBuildHasher {
-            chunk: chunk,
-            fnv_builder: BuildHasherDefault::<FnvHasher>::default(),
+impl <'a> StlBuildHasher<'a> {
+    fn new(data: &'a [u8]) -> Self {
+        StlBuildHasher {
+            data: data,
+            builder: BuildHasherDefault::<FnvHasher>::default(),
         }
     }
 }
 
-impl<'a> BuildHasher for CustomBuildHasher<'a> {
-    type Hasher = CustomHasher<'a>;
+impl<'a> BuildHasher for StlBuildHasher<'a> {
+    type Hasher = StlHasher<'a>;
     fn build_hasher(&self) -> Self::Hasher {
-        CustomHasher {
-            chunk: self.chunk,
-            hasher: self.fnv_builder.build_hasher(),
+        StlHasher {
+            data: self.data,
+            hasher: self.builder.build_hasher(),
         }
     }
 }
 
-struct CustomHasher<'a> {
-    chunk: &'a [u8],
+struct StlHasher<'a> {
+    data: &'a [u8],
     hasher: FnvHasher,
 }
 
-impl<'a> Hasher for CustomHasher<'a> {
+impl<'a> Hasher for StlHasher<'a> {
     fn write(&mut self, bytes: &[u8]) {
         unimplemented!();
     }
     fn write_u32(&mut self, index: u32) {
+        println!("Hashing {} ({:?})", index, vertex_range(index));
+        println!("{:?}", parse_vec(&self.data[vertex_range(index)]));
         let i = index as usize;
-        self.hasher.write(&self.chunk[i..i + 12]);
+        self.hasher.write(&self.data[vertex_range(index)]);
     }
     fn finish(&self) -> u64 {
-        self.hasher.finish()
+        println!("finishing");
+        let out = self.hasher.finish();
+        println!("{}", out);
+        out
     }
 }
+
+fn vertex_range(v: u32) -> std::ops::Range<usize> {
+    let triangle = (v / 3) as usize;
+    let vertex = (v % 3) as usize;
+    let i = 80
+        + size_of::<u32>()
+        + triangle * (3 * 4 * size_of::<f32>() + size_of::<u16>())
+        + 3 * size_of::<f32>()
+        + vertex * 3 * size_of::<f32>();
+    i..i + 3 * size_of::<f32>()
+}
+
+struct StlKey<'a> {
+    v: u32,
+    data: &'a [u8]
+}
+
+impl<'a> PartialEq for StlKey<'a> {
+    fn eq(&self, other: &StlKey) -> bool {
+        self.data[vertex_range(self.v)] == self.data[vertex_range(other.v)]
+    }
+}
+
+impl Borrow<StlKey> for u32 {
+    fn borrow(&self) -> &Id {
+        &self.id
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 struct Worker<'a> {
     chunk: &'a [u8],
@@ -121,24 +156,24 @@ impl <'a> Worker <'a> {
     }
 }
 
-fn load_from_chunk<'a>(data: &'a [u8]) {
-    let hasher = CustomBuildHasher::new(data);
-    let mut set = HashSet::<u32, _>::with_hasher(hasher);
-    set.insert(10);
-    println!("{:?}", set.contains(&10));
-    println!("{:?}", set.contains(&11));
-}
-
 fn main() -> Result<(), Error> {
-    let file = File::open("/Users/mkeeter/Models/porsche.stl")?;
+    //let file = File::open("/Users/mkeeter/Models/porsche.stl")?;
+    let file = File::open("cube.stl")?;
     let mmap = unsafe { MmapOptions::new().map(&file)? };
     println!("Loading stl");
 
     let header = parse_header(&mmap)
         .map_err(|s| err_msg(format!("{:?}", s)))?;
-    println!("{:?}", (header.1).1);
+    let triangle_count = (header.1).1;
+    println!("Triangle count: {}", triangle_count);
 
-    load_from_chunk(&mmap);
+    let hasher = StlBuildHasher::new(&mmap);
+    let mut set = HashSet::<u32, _>::with_hasher(hasher);
+    for v in 0..triangle_count * 3 {
+        let k = StlKey { v: v, data: &mmap };
+        set.get(&k);
+    }
+    println!("total vertex count: {}", set.len());
 
     Ok(())
 }
