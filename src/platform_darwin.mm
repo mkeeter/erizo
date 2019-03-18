96694d6049765766b0bbb9cb4338d5b21fbb9436
Matt Keeter <matt.j.keeter@gmail.com>
2019-03-17 22:10:21 -0400

    Make dragging STL to icon work

--- src/platform_darwin.mm
+++ src/platform_darwin.mm
@@ -7,6 +7,8 @@ extern "C" {
 
 #import <Cocoa/Cocoa.h>
 #import <Foundation/Foundation.h>
+#import <Foundation/NSObjCRuntime.h>
+#import <objc/runtime.h>
 
 @interface Glue : NSObject {
 @public
@@ -40,10 +42,28 @@ extern "C" {
 }
 @end
 
+static Glue* GLUE_INSTANCE=NULL;
+
+void fopenFiles(id self, SEL _cmd, NSApplication* application,
+                NSArray<NSString *>* openFiles) {
+    for (NSString* t in openFiles) {
+        NSLog(@"opening %@ in %p", t, (void*)GLUE_INSTANCE->app);
+        instance_t* instance = app_open(GLUE_INSTANCE->app, [t UTF8String]);
+        NSWindow* window = glfwGetCocoaWindow(instance->window);
+        [window makeKeyWindow];
+    }
+    NSLog(@"%@ %@ %@", self, application, openFiles);
+}
+
 extern "C" void platform_init(app_t* app)
 {
-    Glue *glue = [[Glue alloc] init];
-    glue->app = app;
+    NSLog(@"Application is at %p", (void*)app);
+    GLUE_INSTANCE = [[Glue alloc] init];
+    GLUE_INSTANCE->app = app;
+
+    Class delegate = object_getClass([NSApp delegate]);
+    class_addMethod(delegate, @selector(application:openFiles:),
+                    (IMP)fopenFiles, "v@:@@");
 
     NSMenu *fileMenu = [[[NSMenu alloc] initWithTitle:@"File"] autorelease];
     NSMenuItem *fileMenuItem = [[[NSMenuItem alloc]
@@ -53,13 +73,13 @@ extern "C" void platform_init(app_t* app)
     NSMenuItem *open = [[[NSMenuItem alloc]
         initWithTitle:@"Open"
         action:@selector(onOpen) keyEquivalent:@"o"] autorelease];
-    open.target = glue;
+    open.target = GLUE_INSTANCE;
     [fileMenu addItem:open];
 
     NSMenuItem *close = [[[NSMenuItem alloc]
         initWithTitle:@"Close"
         action:@selector(onClose) keyEquivalent:@"w"] autorelease];
-    close.target = glue;
+    close.target = GLUE_INSTANCE;
     [fileMenu addItem:close];
 
     [[NSApplication sharedApplication].mainMenu insertItem:fileMenuItem atIndex:1];
