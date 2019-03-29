66548f1c869cf0f376ee77d223f842fb977262d2
Matt Keeter <matt.j.keeter@gmail.com>
2019-03-29 18:09:23 -0400

    Cleaning macros

--- inc/log.h
+++ inc/log.h
@@ -7,11 +7,11 @@ typedef enum {
 
 void log_print(log_type_t t, const char* file, int line, const char *fmt, ...);
 
-#define log_trace(...)  log_print(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__);
-#define log_info(...)   log_print(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__);
-#define log_warn(...)   log_print(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__);
-#define log_error(...)  log_print(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__);
+#define log_trace(...)  log_print(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
+#define log_info(...)   log_print(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
+#define log_warn(...)   log_print(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
+#define log_error(...)  log_print(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 #define log_error_and_abort(...) do {                           \
     log_print(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__);      \
     exit(-1);                                                   \
-} while (0);
+} while (0)
