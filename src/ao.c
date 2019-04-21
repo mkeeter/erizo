b8532d52bc27c9fa08a1156b6b9abe167b0aeb69
Matt Keeter <matt.j.keeter@gmail.com>
2019-04-21 15:39:22 -0400

    Record Z as depth

--- src/ao.c
+++ src/ao.c
@@ -19,7 +19,7 @@ static const GLchar* AO_FS_SRC = GLSL(330,
 out vec4 out_color;
 
 void main() {
-    out_color = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+    out_color = vec4(gl_FragCoord.z, 0.0f, 0.0f, 0.0f);
 });
 
 ao_t* ao_new(unsigned render_size, unsigned vol_logsize) {
